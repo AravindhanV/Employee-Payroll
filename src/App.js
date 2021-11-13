69fa96d14d88103671223377b53a14121fa5c3a0
Aravindhan V <aravindhan.v@perfios.com>
2021-11-13 09:45:29 +0530

    ADDED: Home page to display all employees

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PayrollForm from './components/payroll-form/payroll-form';
+import Home from './components/home/home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<PayrollForm />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
